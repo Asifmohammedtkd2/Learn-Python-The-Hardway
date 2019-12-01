578a9e0b45d2d2589979d35b1033b1e7cbd55816
Asifmohammedtkd2 <asifmohammedn77@gmail.com>
2019-12-01 16:59:42 +0530

    Printing second way is by using triple-quotes

---
+++ ex10.py
@@ -0,0 +1,14 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat ="I'm split\non a line."
+backslash_cat = "i'm \\a\\cat."
+
+fat_cat =""" 
+I'll do a list:
+\t* Cat food
+\t* Fishes 
+\t* catnip\n\t* Grass
+"""
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
