870874a896fb57e0a77425af8716cc93eb56ce1f
Asifmohammedtkd2 <asifmohammedn77@gmail.com>
2019-12-01 23:46:02 +0530

    pass three command line arguments

---
+++ ex13.py
@@ -0,0 +1,7 @@
+from sys import argv
+# read the WYSS selection for how to run this
+script, first, second, third = argv
+print("The script is called:",script)
+print("Your first variable is:",first)
+print("Your second variable is:",second)
+print("Your third variable is:",third)
